57b660d16056f350d763e231c91381f6745bccf0
todayIsDecember <timkivit@gmail.com>
2023-09-03 00:08:37 +0300

    show in console

---
+++ services/log.service.js
@@ -0,0 +1,24 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+const printError = (error) => {
+  console.log(`${chalk.bgRed(" ERROR ")} ${error}`);
+};
+
+const printSuccess = (message) => {
+  console.log(`${chalk.bgGreen(" SUCCESS ")} ${message}`);
+};
+
+const printHelp = () => {
+  console.log(
+    dedent`
+      ${chalk.bgCyan(" HELP ")}
+      Без параметрів -- вивід погоди
+      -h -- допомога
+      -s -- задати місто
+      -t -- задати токен
+    `
+  );
+};
+
+export { printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
 import { getArguments } from "./helpers/arg_helpers.js";
+import { printError, printHelp, printSuccess } from "./services/log.service.js";
 
 const initCLI = () => {
   const args = getArguments(process.argv);
-  console.log(args);
+  if (args.h) {
+    printHelp();
+  }
+  if (args.s) {
+  }
+  if (args.t) {
+  }
 };
 
 initCLI();
